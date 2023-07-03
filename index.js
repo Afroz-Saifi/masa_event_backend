abb4019250cb3e0a71bd86394671a2d6fb507967
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-07-03 12:05:23 +0530

    updated backend

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("iii");
+const jsonServer = require("json-server")
+const server = jsonServer.create()
+const router = jsonServer.router("db.json")
+const middleware = jsonServer.defaults()
+const PORT = 8080;
+
+server.use(middleware)
+server.use(router)
+
+server.listen(PORT)
